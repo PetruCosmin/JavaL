e21341963be798cd0d9a9914e90148531ec130df
petru <petru0cosmin@gmail.com>
2022-12-22 14:00:03 +0200

    proiect

---
+++ CitireXlsx/Main.java
@@ -0,0 +1,111 @@
+package org.example;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void readXLSXFile() throws IOException {
+        InputStream ExcelFileToRead = new FileInputStream("C:\\xls\\red.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
+
+
+
+        XSSFSheet sheet = wb.getSheetAt(0);
+        XSSFRow row;
+        XSSFCell cell;
+
+        Iterator<Row> rows = sheet.rowIterator();
+
+        while (rows.hasNext()) {
+            row = (XSSFRow) rows.next();
+            Iterator<Cell> cells = row.cellIterator();
+            while (cells.hasNext()) {
+                cell = (XSSFCell) cells.next();
+
+                if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+                    System.out.print(cell.getStringCellValue() + " ");
+                } else if (cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
+                    System.out.print(cell.getNumericCellValue() + " ");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void citire() throws IOException {
+        File myFile = new File("C:\\xls\\red.xlsx");
+        FileInputStream fis = new FileInputStream(myFile);
+        // Finds the workbook instance for XLSX file
+        XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
+        // Return first sheet from the XLSX workbook
+        XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+        // Get iterator to all the rows in current sheet
+        Iterator<Row> rowIterator = mySheet.iterator();
+        // Traversing over each row of XLSX file
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            // For each row, iterate through each columns
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                switch (cell.getCellType()) {
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(cell.getStringCellValue() + "\t");
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print(cell.getNumericCellValue() + "\t");
+                        break;
+                    case Cell.CELL_TYPE_BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue() + "\t");
+                        break;
+                    default:
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void writeXLSXFile() throws IOException {
+        String excelFileName = "C:\\xls\\Registru.xlsx";//name of excel file
+        String sheetName = "Sheet1";//name of sheet
+        XSSFWorkbook wb = new XSSFWorkbook(sheetName);
+        XSSFSheet sheet = wb.createSheet(sheetName);
+        //iterating r number of rows
+        for (int r = 0; r < 100; r++) {
+            XSSFRow row = sheet.createRow(r);
+            //iterating c number of columns
+            for (int c = 0; c < 20; c++) {
+                XSSFCell cell = row.createCell(c);
+                cell.setCellValue("Cell " + r + " " + c);
+            }
+        }
+        FileOutputStream fileOut = new FileOutputStream(excelFileName);
+        //write this workbook to an Outputstream.
+        wb.write(fileOut);
+        fileOut.flush();
+        fileOut.close();
+    }
+        public static void main(String[] args)  {
+            try {
+                readXLSXFile();
+                citire();
+                writeXLSXFile();
+
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
+}
